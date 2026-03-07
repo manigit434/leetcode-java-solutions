c6bedda93dca5d0157eaa9c58fcce4ff321b9e49
Manikanta Katta <kattamanikanta434@gmail.com>
2026-03-07 12:38:04 +0530

    find intersection node of two linked lists [easy]

---
+++ linkedlists/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,16 @@
+// Time: O(m+n), Space: O(1)
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA, b = headB;
+
+        // Traverse both lists, switch heads when reaching the end
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
+        }
+
+        return a; // null if no intersection or the intersection node
+    }
+}
