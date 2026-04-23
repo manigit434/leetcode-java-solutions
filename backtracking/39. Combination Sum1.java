3377671b2f2cfed5671e9871dc351ede560c75f7
Manikanta Katta <kattamanikanta434@gmail.com>
2026-04-23 20:10:35 +0530

    Implement Combination Sum algorithm [Medium]

---
+++ backtracking/39. Combination Sum1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
+        if (remain < 0) return;
+        else if (remain == 0) result.add(new ArrayList<>(tempList));
+        else {
+            for (int i = start; i < candidates.length; i++) {
+                tempList.add(candidates[i]);
+                backtrack(result, tempList, candidates, remain - candidates[i], i);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+}
+// Time Complexity: O(N^(T/M + 1)), Space Complexity: O(T/M)
