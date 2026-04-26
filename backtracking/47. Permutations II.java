651cc07327cb674596aa7411b959f5f8d960a240
Manikanta Katta <kattamanikanta434@gmail.com>
2026-04-26 11:57:22 +0530

    Add unique permutations solution in Permutations II [Medium]
    Implement a solution to generate unique permutations of an array of integers.

---
+++ backtracking/47. Permutations II.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, boolean[] used) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])) continue;
+            used[i] = true;
+            temp.add(nums[i]);
+            backtrack(res, temp, nums, used);
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
+// Time Complexity: O(n * n!) - Generating unique permutations of length n.
+// Space Complexity: O(n * n!) - Storage for the result list and recursion stack.
