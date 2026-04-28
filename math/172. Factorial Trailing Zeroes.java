097ece49c826bbef4e95c0251b3f5ac108a00dee
Manikanta Katta <kattamanikanta434@gmail.com>
2026-04-28 19:57:29 +0530

    Add solution for trailing zeroes in factorial [Medium]
    Implement a solution to calculate the number of trailing zeroes in the factorial of a number.

---
+++ math/172. Factorial Trailing Zeroes.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n >= 5) {
+            count += n / 5;
+            n /= 5;
+        }
+        return count;
+    }
+}
+// Time Complexity: O(log5 N)
+// Space Complexity: O(1)
