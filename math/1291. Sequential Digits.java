947196c5bfebe867eeed8e473010d1118c069358
Manikanta Katta <kattamanikanta434@gmail.com>
2026-07-13 09:46:39 +0530

    Add solution for finding sequential digits [Medium]
    Implement a solution to find sequential digits within a given range.

---
+++ math/1291. Sequential Digits.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new ArrayList<>();
+        String sample = "123456789";
+        
+        int minLen = String.valueOf(low).length();
+        int maxLen = String.valueOf(high).length();
+        
+        for (int len = minLen; len <= maxLen; len++) {
+            for (int start = 0; start <= 9 - len; start++) {
+                String sub = sample.substring(start, start + len);
+                int num = Integer.parseInt(sub);
+                
+                if (num >= low && num <= high) {
+                    result.add(num);
+                }
+            }
+        }
+        
+        return result;
+    }
+}
