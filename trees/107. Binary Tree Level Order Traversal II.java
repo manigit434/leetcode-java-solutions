f531a2d6a2c9e77f2f9c6bd4733b7b7d466a04f4
Manikanta Katta <kattamanikanta434@gmail.com>
2026-08-31 20:56:19 +0530

    Implement bottom-up level order traversal for binary tree [Medium]

---
+++ trees/107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        
+        // Reverse the top-down result to get bottom-up order
+        Collections.reverse(result);
+        return result;
+    }
+}
