bb13522b03942aa2e71952fae7b32dcc0c115e8c
Manikanta Katta <kattamanikanta434@gmail.com>
2026-09-02 23:47:10 +0530

    Add Solution class for uniform parity array check [Easy]

---
+++ arrays/3875. Construct Uniform Parity Array I.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean checkIfCanConstruct(int[] nums1) {
+        // Based on parity rules, it is mathematically always possible 
+        // to construct a uniform parity array from distinct integers.
+        return true;
+    }
+}
